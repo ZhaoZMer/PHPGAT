71bdc801dc4b7545e9d00bdb0b7c4075d160c871
ZhaoZMer <754506029@qq.com>
2024-07-21 20:14:35 +0800

    Update create_feature.py

--- create_feature.py
+++ create_feature.py
@@ -5,23 +5,6 @@ from Bio import SeqIO
 
 os.makedirs('node_feature/', exist_ok=True)
 
-# def node2id(file_in_fn):
-#     file_list = os.listdir(file_in_fn)
-#     file2idx = {}
-#     for idx, file in enumerate(file_list):
-#         file2idx[file.rsplit('.', 1)[0]] = idx
-#     return file2idx
-#
-#
-# virus2id = node2id('train_phage/')
-# pkl.dump(virus2id, open('node_feature/phages.dict', 'wb'))
-#
-# prokaryote2id = node2id('prokaryote/')
-# pkl.dump(prokaryote2id, open('node_feature/prokaryote.dict', 'wb'))
-#
-# test_virus2id = node2id('single_contig/')
-# pkl.dump(test_virus2id, open('node_feature/test_phages.dict', 'wb'))
-
 def return_4mer(file_in_fn):
     # alphbet
     k_list = ["A", "C", "G", "T"]
@@ -36,27 +19,27 @@ def return_4mer(file_in_fn):
     file_list = os.listdir(file_in_fn)
     num_file = len(file_list)
     file2idx = {}
-    feature = np.zeros((num_file, 256))  #创建一个全零矩阵
-    for idx, file in enumerate(file_list):  #遍历文件列表，同时给每个文件一个唯一的索引
-        file2idx[file.rsplit('.', 1)[0]] = idx  #使用file2idx字典存储每个文件的名字和它对应的索引
-        for record in SeqIO.parse(file_in_fn + file, 'fasta'):  #读取Fasta文件
-            seq = str(record.seq)  #转换为字符串
-            seq = seq.upper()  #转换为大写，以确保碱基的一致性
-            for pos in range(len(seq)-3):   #遍历每个序列，以4-mer为窗口
+    feature = np.zeros((num_file, 256))  
+    for idx, file in enumerate(file_list):  
+        file2idx[file.rsplit('.', 1)[0]] = idx  
+        for record in SeqIO.parse(file_in_fn + file, 'fasta'):  
+            seq = str(record.seq)  
+            seq = seq.upper()
+            for pos in range(len(seq)-3):  
                 try:
-                    feature[idx][mer2dict[seq[pos:pos+4]]] += 1  #若找到了该索引，则在该位置的计数加1；若pos=0，指从第1个元素开始取到第4个（[0,4]）
+                    feature[idx][mer2dict[seq[pos:pos+4]]] += 1  
                 except:
                     #print(seq[pos:pos+4])
-                    pass    #若碱基序列不在字典中，则跳过
+                    pass    
     # nomarlization  归一化
     norm_feature = np.zeros((num_file, 256))
     for i in range(len(feature)):
-        norm_feature[i] = (feature[i] - np.min(feature[i]))/(np.max(feature[i]) - np.min(feature[i]))  #归一化公式：（X-Xmin）/（Xmax-Xmin） np.min(feature[i])指第i行中的最小值
+        norm_feature[i] = (feature[i] - np.min(feature[i]))/(np.max(feature[i]) - np.min(feature[i])) 
     return norm_feature, file2idx
 
 phages, phages2id = return_4mer('train_phage/')
-pkl.dump(phages2id, open('node_feature/phages.dict', 'wb'))  #保存位置索引
-pkl.dump(phages, open('node_feature/phages.F', 'wb'))   #保存特征矩阵
+pkl.dump(phages2id, open('node_feature/phages.dict', 'wb'))  
+pkl.dump(phages, open('node_feature/phages.F', 'wb'))   
 
 prokaryote, prokaryote2id = return_4mer('prokaryote/')
 pkl.dump(prokaryote2id, open('node_feature/prokaryote.dict', 'wb'))
